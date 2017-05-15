2f0641f2c2204c2c181bae700ddc14de557e1a8c
kraevskii-m <kramihm@gmail.com>
2017-05-15 12:45:17 +0300

    Cleaned the template

---
+++ SteelUnicorn/FencesCommands.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(SteelUnicorn.FencesCommands))]
+
+namespace SteelUnicorn
+{
+    public class FencesCommands
+    {
+        [CommandMethod("CreateFence", CommandFlags.Modal)]
+        public void CreateFence()
+        {
+
+        }
+    }
+
+}
